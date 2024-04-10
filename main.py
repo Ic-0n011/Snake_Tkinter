acaa31670f14de39fe2536d97049fe91693beec4
Ic-0n0111 <113190401+Ic-0n011@users.noreply.github.com>
2024-04-10 22:08:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ CANVAS_BG = 'pink'
 LINE_COLOR = 'black'
 TILE_SIZE = 32
 SNAKE_COLOR = 'green'
+TAIL_COLOR = '#7FFF00'
 FPS = 15
 
 
@@ -57,31 +58,35 @@ class App:
                 i * TILE_SIZE,
                 fill=LINE_COLOR
             )
+    
+    def end_game(self):
+        print("конец")
+        self.window.destroy()
 
 
 class Game:
     def __init__(self, canvas: tkinter.Canvas, size: int) -> None:
         self.canvas = canvas
         self.size = size
-        print(size)
         self.snake = Snake(
             self.size // 2 * TILE_SIZE,
             self.size // 2 * TILE_SIZE,
             TILE_SIZE,
             self.canvas,
             SNAKE_COLOR,
-            'up',
-            'down',
-            'left',
-            'right'
+            key_up='Up',
+            key_down='Down',
+            key_left='Left',
+            key_right='Right'
         )
         self.update()
 
     def update(self) -> None:
         self.canvas.delete('snake')
+        self.canvas.delete('tail')
         self.snake.move()
         self.snake.draw()
-        self.canvas.after(1000 // FPS, self.update)
+        self.canvas.after(1000 // FPS * 2, self.update)
 
     def on_key(self, event: tkinter.Event):
         self.snake.on_key(event)
@@ -110,6 +115,20 @@ class Snake:
         self.key_left = key_left
         self.key_right = key_right
         self.direction = (1, 0)
+        self.segments = dict()
+        for _ in range(3):
+            self.create_new_segment()
+    
+    def create_new_segment(self):
+        self.segments[len(self.segments)+1] = Tail(
+            self,
+            self.col,
+            self.row,
+            self.size,
+            self.canvas,
+            TAIL_COLOR,
+            len(self.segments)+1
+            )
 
     def draw(self) -> None:
         self.canvas.create_rectangle(
@@ -122,6 +141,11 @@ class Snake:
         )
 
     def move(self):
+        if len(self.segments) > 0:
+            self.segments[1].next_col = self.col
+            self.segments[1].next_row = self.row
+            self.segments[1].move()
+            self.segments[1].draw()
         self.col += self.size * self.direction[0]
         self.row += self.size * self.direction[1]
 
@@ -138,6 +162,46 @@ class Snake:
         if event.keysym == self.key_right:
             self.change_direction((1, 0))
 
+
+class Tail():
+    def __init__(
+            self,
+            snake: Snake,
+            col: int,
+            row: int,
+            size: int,
+            canvas: tkinter.Canvas,
+            color: str,
+            number_segment: int
+    ) -> None:
+        self.snake = snake
+        self.col = col
+        self.row = row
+        self.size = size
+        self.canvas = canvas
+        self.color = color
+        self.number_segmen = number_segment
+        self.next_col = col
+        self.next_row = row
+    
+    def draw(self) -> None:
+        self.canvas.create_rectangle(
+            self.col,
+            self.row,
+            self.col + self.size,
+            self.row + self.size,
+            fill=self.color,
+            tags='tail'
+        )
+
+    def move(self):
+        if len(self.snake.segments) > self.number_segmen:
+            self.snake.segments[self.number_segmen+1].next_col = self.col
+            self.snake.segments[self.number_segmen+1].next_row = self.row
+            self.snake.segments[self.number_segmen+1].move()
+            self.snake.segments[self.number_segmen+1].draw()
+        self.col = self.next_col
+        self.row = self.next_row
 
 class Food:
     pass
