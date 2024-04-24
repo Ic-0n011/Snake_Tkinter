af046ac4c9de364a3c2cc985c43865f655e9a3d8
Ic-0n0111 <113190401+Ic-0n011@users.noreply.github.com>
2024-04-24 19:42:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ TILE_SIZE = 30
 SNAKE_COLOR = 'green'
 TAIL_COLOR = '#7FFF00'
 FOOD_COLOR = 'red'
+MENU_TEXT_COLOR = 'black'
+MENU_FONT_NAME = 'Impact'
 FPS = 15
 
 
@@ -20,7 +22,6 @@ class App:
         self.height = self.window.winfo_screenheight()
         self.canvas_size = min((self.width, self.height))
         self.tiles_ammount = self.canvas_size // TILE_SIZE
-        self.window.bind('<Key>', self.on_key)
         self.canvas = tkinter.Canvas(
             self.window,
             width=self.width // TILE_SIZE * TILE_SIZE,
@@ -28,18 +29,13 @@ class App:
             bg=CANVAS_BG,
             highlightthickness=0
         )
+        
         self.canvas.pack()
         self.draw_lines()
         self.max_col = self.width // TILE_SIZE * TILE_SIZE
         self.max_row = self.height // TILE_SIZE * TILE_SIZE
         self.game = Game(self.canvas, self.tiles_ammount, self.max_col, self.max_row)
         self.window.mainloop()
-
-    def on_key(self, event: tkinter.Event) -> None:
-        if event.keysym == 'Escape':
-            self.window.destroy()
-        else:
-            self.game.on_key(event)
 
     def draw_lines(self) -> None:
         '''Делит поле на клетки вериткальными и горизонтальными линиями'''
@@ -61,19 +57,35 @@ class App:
                 fill=LINE_COLOR
             )
 
-    def end_game(self):
-        print("конец")
-        self.window.destroy()
-
 
 class Game:
     def __init__(self, canvas: tkinter.Canvas, size: int, max_col, max_row) -> None:
         self.canvas = canvas
+        self.canvas.bind('<Key>', self.on_key)
+        self.canvas.focus_set()
         self.size = size
         self.col = max_col // TILE_SIZE
         self.row = max_row // TILE_SIZE
         self.max_col = max_col
         self.max_row = max_row
+        self.min_size_font = int(min((self.max_col, self.max_row)) * 0.1)
+        self.snake = None
+        self.food = None
+        self.is_run = False
+        self.show_menu()
+
+    def show_menu(self) -> None:
+        self.canvas.create_text(
+            self.max_col // 2,
+            self.max_row // 2,
+            text='ENTER - играть\nESKAPE - выход',
+            fill=MENU_TEXT_COLOR,
+            font=(MENU_FONT_NAME, self.min_size_font),
+            tags='text_menu'
+            )
+
+    def start(self) -> None:
+        self.is_run = True
         self.snake = Snake(
             self.size // 2 * TILE_SIZE,
             self.size // 2 * TILE_SIZE,
@@ -86,39 +98,43 @@ class Game:
             key_left='Left',
             key_right='Right'
         )
-        self.food = None
+        if not self.food:
+            self.food = Food(
+                self.canvas,
+                random.randint(1, self.col) * TILE_SIZE,
+                random.randint(1, self.row) * TILE_SIZE
+            )
+        self.canvas.delete(self.snake.tag, self.food.tag)
         self.update()
 
     def update(self) -> None:
         self.snake.collide_border()
         self.snake.collide_tail()
         if not self.food:
-            clean_cell = []
-            for col in range(self.max_col//TILE_SIZE):
-                col = col * TILE_SIZE
-                for row in range(self.max_row//TILE_SIZE):
-                    row = row * TILE_SIZE
-                    if col != self.snake.col and row != self.snake.row:
-                        for i in self.snake.segments:
-                            if col != self.snake.segments[i].col and row != self.snake.segments[i].row:
-                                clean_cell.append[(col, row)]
-            food_cord = random.choice(clean_cell)
             self.food = Food(
                 self.canvas,
-                food_cord[0],
-                food_cord[1]
+                random.randint(1, self.col) * TILE_SIZE,
+                random.randint(1, self.row) * TILE_SIZE
             )
         self.canvas.delete('snake')
         self.canvas.delete('food')
         self.food.draw()
         self.snake.draw()
         if self.snake.is_move:
+            self.snake.change_direction()
             self.snake.move()
             self.food = self.snake.eating(self.food)
+
         self.canvas.after(132, self.update)
 
     def on_key(self, event: tkinter.Event):
-        self.snake.on_key(event)
+        if event.keysym == 'Escape':
+            self.canvas.winfo_toplevel().destroy()
+        elif event.keysym == 'Return':
+            if not self.is_run:
+                self.start()
+        else:
+            self.snake.on_key(event)
 
 
 class Snake:
@@ -135,6 +151,7 @@ class Snake:
             key_left: str,
             key_right: str
     ) -> None:
+        self.tag = 'snake'
         self.is_move = True
         self.col = col
         self.row = row
@@ -146,6 +163,7 @@ class Snake:
         self.key_down = key_down
         self.key_left = key_left
         self.key_right = key_right
+        self.last_key = None
         self.direction = (1, 0)
         self.segments = dict()
 
@@ -166,35 +184,40 @@ class Snake:
             self.col + TILE_SIZE,
             self.row + TILE_SIZE,
             fill=self.color,
-            tags='snake'
+            tags=self.tag
         )
 
     def move(self):
+        self.col += TILE_SIZE * self.direction[0]
+        self.row += TILE_SIZE * self.direction[1]
         if len(self.segments) > 0:
             self.segments[1].draw()
             self.segments[1].next_col = self.col
             self.segments[1].next_row = self.row
             self.segments[1].move()
-        self.col += TILE_SIZE * self.direction[0]
-        self.row += TILE_SIZE * self.direction[1]
 
-    def change_direction(self, direction):
-        if self.direction[0] == -direction[0]:
+    def change_direction(self) -> None:
+        if self.last_key == self.key_up:
+            new_direction = (0, -1)
+        elif self.last_key == self.key_down:
+            new_direction = (0, 1)
+        elif self.last_key == self.key_left:
+            new_direction = (-1, 0)
+        elif self.last_key == self.key_right:
+            new_direction = (1, 0)
+        else:
             return
-        if self.direction[1] == -direction[1]:
+
+        if self.direction[0] == -new_direction[0]:
             return
-        self.direction = direction
+        if self.direction[1] == -new_direction[1]:
+            return
+
+        self.direction = new_direction
+        self.last_key = None
 
     def on_key(self, event: tkinter.Event):
-        if event.keysym == self.key_up:
-            direction = (0, -1)
-        elif event.keysym == self.key_down:
-            direction = (0, 1)
-        elif event.keysym == self.key_left:
-            direction = (-1, 0)
-        elif event.keysym == self.key_right:
-            direction = (1, 0)
-        self.change_direction(direction)
+        self.last_key = event.keysym
 
     def eating(self, food):
         if self.col == food.col and self.row == food.row:
@@ -261,6 +284,7 @@ class Tail():
 
 class Food:
     def __init__(self, canvas: tkinter.Canvas, col: int, row: int) -> None:
+        self.tag = 'food'
         self.canvas = canvas
         self.col = col
         self.row = row
@@ -273,7 +297,7 @@ class Food:
             self.col + TILE_SIZE,
             self.row + TILE_SIZE,
             fill='red',
-            tags='food'
+            tags=self.tag
         )
 
 
